bfa77cc644ab42fb549ce4c8c31ebfac897d59c8
Srushtika <n.srushtika@gmail.com>
2020-07-21 16:32:12 +0100

    🥇 CSS, HTML & JS updates implementing nickname and individual score

--- app.js
+++ app.js
@@ -1,87 +1,177 @@
-document.addEventListener('DOMContentLoaded' , () => {
-    const bird = document.querySelector('.bird')
-    const gameDisplay = document.querySelector('.game-container')
-    const ground = document.querySelector('.ground-moving')
+const profanityBaseURL = "https://www.purgomalum.com/service/plain?text=";
+const nickNamesDictionary = [
+  "pink crow",
+  "green pigeon",
+  "brown robin",
+  "blue woodpecker",
+  "purple sparrow",
+  "yellow kingfisher",
+  "gray warbler",
+  "orange bulbul",
+  "black drongo",
+  "red seagulls",
+  "beige flamingo",
+  "frost eagles",
+  "fuscia owl",
+  "mint kite",
+  "hickory parakeet",
+  "tortilla beeeater",
+  "wood munia",
+  "violet dove",
+  "eggplant peacock",
+  "golden oriole",
+  "magenta flycatcher",
+  "mulberry quail",
+  "slate magpie",
+  "navy roller",
+  "azure emu",
+  "arctic sunbird",
+  "iris starling",
+  "olive rockthrush",
+  "pecan barnowl",
+  "carob goose",
+  "coal duck",
+  "grease trogon",
+  "raven nightjar",
+  "sepia barbet",
+];
+let obstacleTimers = [];
+let gameStarted = false;
+let gameTimerId;
+let myScore = 0;
+let highScore = 0;
+let highScoreNickname = "anonymous panda";
+let myNickname;
 
-    let birdLeft = 220
-    let birdBottom = 100
-    let gravity = 3
-    let isGameOver = false
-    let gap = 430
+if (localStorage.getItem("flappy-nickname")) {
+  myNickname = localStorage.getItem("flappy-nickname");
+} else {
+  myNickname = nickNamesDictionary[Math.floor(Math.random() * 34)];
+  localStorage.setItem("flappy-nickname", myNickname);
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+  const bird = document.querySelector(".bird");
+  const gameDisplay = document.querySelector(".game-container");
+  const ground = document.querySelector(".ground-moving");
+  let nicknameInput = document.getElementById("nickname-input");
+  let updateNicknameBtn = document.getElementById("update-nickname");
+  let scoreLabel = document.getElementById("score-label");
+  let topScoreLabel = document.getElementById("top-label");
+  let scoreList = document.getElementById("score-list");
 
-    function startGame() {
-        birdBottom -= gravity
-        bird.style.bottom = birdBottom + 'px'
-        bird.style.left = birdLeft + 'px'
+  let birdLeft = 220;
+  let birdBottom = 350;
+  let gravity = 2;
+  let isGameOver = false;
+  let gap = 450;
+
+  const filterNickname = async (nicknameText) => {
+    const http = new XMLHttpRequest();
+    let encodedText = encodeURIComponent(nicknameText);
+    http.open("GET", profanityBaseURL + encodedText + "&fill_text=***");
+    http.send();
+    http.onload = () => {
+      myNickname = http.responseText;
+      nicknameInput.value = myNickname;
+      localStorage.setItem("flappy-nickname", myNickname);
+    };
+  };
+
+  topScoreLabel.innerHTML =
+    "Top score - " + highScore + "pts by " + highScoreNickname;
+  nicknameInput.value = myNickname;
+  updateNicknameBtn.addEventListener("click", () => {
+    filterNickname(nicknameInput.value);
+  });
+
+  window.addEventListener("keydown", function (e) {
+    if (e.keyCode == 32 && e.target == document.body) {
+      e.preventDefault();
     }
-    let gameTimerId = setInterval(startGame, 20)
+  });
 
-    function control(e) {
-        if (e.keyCode === 32) {
-            jump()
+  gameDisplay.onclick = function () {
+    if (!gameStarted) {
+      gameStarted = true;
+      document.addEventListener("keydown", control);
+      gameTimerId = setInterval(startGame, 20);
+      generateObstacles();
+    }
+  };
+
+  function startGame() {
+    birdBottom -= gravity;
+    bird.style.bottom = birdBottom + "px";
+    bird.style.left = birdLeft + "px";
+  }
+
+  function control(e) {
+    if (e.keyCode === 32 && !isGameOver) {
+      jump();
+    }
+  }
+
+  function jump() {
+    if (birdBottom < 500) birdBottom += 50;
+    bird.style.bottom = birdBottom + "px";
+  }
+
+  function generateObstacles() {
+    if (!isGameOver) {
+      let obstacleLeft = 500;
+      let obstacleBottom = Math.random() * 60;
+
+      const obstacle = document.createElement("div");
+      const topObstacle = document.createElement("div");
+      obstacle.classList.add("obstacle");
+      topObstacle.classList.add("topObstacle");
+      gameDisplay.appendChild(obstacle);
+      gameDisplay.appendChild(topObstacle);
+      obstacle.style.left = obstacleLeft + "px";
+      obstacle.style.bottom = obstacleBottom + "px";
+      topObstacle.style.left = obstacleLeft + "px";
+      topObstacle.style.bottom = obstacleBottom + gap + "px";
+      let timerId = setInterval(moveObstacle, 20);
+      obstacleTimers.push(timerId);
+      function moveObstacle() {
+        obstacleLeft -= 2;
+        obstacle.style.left = obstacleLeft + "px";
+        topObstacle.style.left = obstacleLeft + "px";
+        if (obstacleLeft === 220) {
+          myScore++;
+          scoreLabel.innerHTML = "Score: " + myScore;
         }
-    }
-
-    function jump() {
-        if (birdBottom < 500) birdBottom += 50
-        bird.style.bottom = birdBottom + 'px'
-        console.log(birdBottom)
-    }
-    document.addEventListener('keyup', control)
-
-
-    function generateObstacle() {
-        let obstacleLeft = 500
-        let randomHeight = Math.random() * 60
-        let obstacleBottom = randomHeight
-        const obstacle = document.createElement('div')
-        const topObstacle = document.createElement('div')
-        if (!isGameOver) {
-            obstacle.classList.add('obstacle')
-            topObstacle.classList.add('topObstacle')
+        if (obstacleLeft === -50) {
+          clearInterval(timerId);
+          gameDisplay.removeChild(obstacle);
+          gameDisplay.removeChild(topObstacle);
         }
-        gameDisplay.appendChild(obstacle)
-        gameDisplay.appendChild(topObstacle)
-        obstacle.style.left = obstacleLeft + 'px'
-        topObstacle.style.left = obstacleLeft + 'px'
-        obstacle.style.bottom = obstacleBottom + 'px'
-        topObstacle.style.bottom = obstacleBottom + gap + 'px'
-
-        function moveObstacle() {
-            obstacleLeft -=2
-            obstacle.style.left = obstacleLeft + 'px'
-            topObstacle.style.left = obstacleLeft + 'px'
-
-            if (obstacleLeft === -60) {
-                clearInterval(timerId)
-                gameDisplay.removeChild(obstacle)
-                gameDisplay.removeChild(topObstacle)
-            }
-            if (
-                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -200)||
-                birdBottom === 0 
-                ) {
-                gameOver()
-                clearInterval(timerId)
-            }
+        if (
+          (obstacleLeft > 200 &&
+            obstacleLeft < 280 &&
+            birdLeft === 220 &&
+            (birdBottom < obstacleBottom + 210 ||
+              birdBottom > obstacleBottom + gap - 150)) ||
+          birdBottom === 0
+        ) {
+          for (timer in obstacleTimers) {
+            clearInterval(obstacleTimers[timer]);
+          }
+          gameOver();
+          isGameOver = true;
         }
-        let timerId = setInterval(moveObstacle, 20) 
-        if (!isGameOver) setTimeout(generateObstacle, 3000)
-
+      }
+      setTimeout(generateObstacles, 3000);
     }
-    generateObstacle()
+  }
 
-
-    function gameOver() {
-        clearInterval(gameTimerId)
-        console.log('game over')
-        isGameOver = true
-        document.removeEventListener('keyup', control)
-        ground.classList.add('ground')
-        ground.classList.remove('ground-moving')
-    }
-
-
-})
+  function gameOver() {
+    scoreLabel.innerHTML += " | Game Over";
+    clearInterval(gameTimerId);
+    isGameOver = true;
+    document.removeEventListener("keydown", control);
+    ground.classList.add("ground");
+    ground.classList.remove("ground-moving");
+  }
+});
